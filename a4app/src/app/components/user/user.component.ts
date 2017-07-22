be7e121c26b2165a14f30aec656c446b2e2ae69f
chunglerhj <chung.le.rhj@gmx.com>
2017-07-22 20:52:54 +0700

    Added user.service and about.component

--- a4app/src/app/components/user/user.component.ts
+++ a4app/src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -9,13 +10,50 @@ export class UserComponent implements OnInit {
   name: string;
   age: number;
   email: string;
+  address: Address;
+  hobbies: string[];
+  posts: Post[];
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.name = "Chung Le"
     this.age = 28;
     this.email = "chung.le.rhj@gmx.com";
+    this.address = {
+      street: "Nguyen",
+      city: "Hanoi",
+      state: "Vietnam"
+    }
+    this.hobbies = [
+      "Coding",
+      "Playing"
+    ]
+
+    this.userService.getData().subscribe((posts) => {
+      this.posts = posts;
+    })
   }
 
+  onClick() {
+    this.age++;
+  }
+
+  addHobby(hobby) {
+    this.hobbies.push(hobby);
+    return false;
+  }
+
+}
+
+interface Address {
+  street: string,
+  city: string,
+  state: string
+}
+
+interface Post {
+  id: number,
+  title: string,
+  body: string
 }
